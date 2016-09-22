f89f7126fce48e42d02e9ddca68b9f5cbec390b1
hamlet3 <hamletreyes3@gmail.com>
2016-09-22 02:05:25 -0600

    subir

---
+++ Tarea/Tarea/app/Site1.Master.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using BLL;
+
+namespace AutoReyes.app
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+
+            Mensajes mensaje = new Mensajes();
+        
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void EmailTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void EnviarBtn_Click(object sender, EventArgs e)
+        {
+            mensaje.Email=EmailTxt.Text;
+            mensaje.Asunto=AsuntoTxt.Text;
+            mensaje.Mensaje= mensajetxt.Text;
+            mensaje.Insertar();
+        }
+
+        protected void MenjaseTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
